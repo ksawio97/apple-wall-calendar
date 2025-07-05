9a6fcec402cf12a8325fd903177c21731b7f3553
Ksawery <ksawery9708@gmail.com>
2025-07-05 19:09:24 +0200

    fix: there's no longer key problems while rendering

--- frontend/src/components/Calendar/WeekGrid.tsx
+++ frontend/src/components/Calendar/WeekGrid.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import DayModel from "../../models/DayModel"
 import EventGroupsService from "../../services/EventGroupsService"
 import isOnTheSameDate from "../../utils/isOnTheSameDate";
@@ -17,10 +18,10 @@ export default function WeekGrid({ week, currDay, eventGroupService, layer, week
             {week.map((day) => {
                 const marked = isOnTheSameDate(day.day, currDay);
 
-                return (<>
-                        <DayBox key={day.day.toString()} dayModel={day} marked={marked}></DayBox>
+                return (<React.Fragment key={day.day.toString()}>
+                        <DayBox  dayModel={day} marked={marked}></DayBox>
                         <EventsGroupGridWrap groupKey={day.toString()} dayModel={day} activeGroupEvents={eventGroupService.getActiveEvents(day.groupId ?? "", layer)} marked={marked} groupLayer={eventGroupService.getGroupLayer(day.groupId ?? "", layer)}></EventsGroupGridWrap>
-                </>)
+                </React.Fragment>)
             })}
         </div>
     );
--- frontend/src/components/Calendar/WeeksGrid.tsx
+++ frontend/src/components/Calendar/WeeksGrid.tsx
@@ -44,7 +44,14 @@ export default function WeeksGrid({ currDay, weeksBefore, weeksAfter }: { currDa
         <div className="w-full">
             {/* show every week in WeekGrid */}
             {splitIntoSevens(days).map((week) => 
-                <WeekGrid week={week} currDay={currDay} eventGroupService={eventGroupService} layer={layer} weeks={weeksBefore + weeksAfter + 1}></WeekGrid>
+                <WeekGrid
+                    key={week[0].day.toISOString()}
+                    week={week}
+                    currDay={currDay}
+                    eventGroupService={eventGroupService}
+                    layer={layer}
+                    weeks={weeksBefore + weeksAfter + 1}
+                />
             )}
         </div>
     );
